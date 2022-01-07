540d7d9ac9997824d4989330e327fe480f0dad49
avadro112 <jkanant@gmail.com>
2022-01-07 18:17:20 +0530

    added_basic

---
+++ template_x_cpp.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+
+
+using namespace std;
+typedef vector<int> vi;
+typedef map<string,int> map_string;
+
+#define fo(i,n) for(int i=0;i<n;++i)
+#define rev(i,k,n) for(int i=k;i<n;++i)
+
+template<typename... T>
+void read(T&...args)
+{
+	((cin>>args),...);
+}
+
+template<typename...T>
+void write(T&&...args)
+{
+	((cout<<args<<" "),...);
+}
+
+int main ()
+{
+
+	return 0;
+}
