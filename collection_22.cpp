d499aba2b3c6654114775b493e0de662fb0ff445
avadro112 <jkanant@gmail.com>
2023-01-02 12:48:31 +0530

    preprocessor added !

--- collection_22.cpp
+++ collection_22.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define all(v) v.begin(),v.end()
+const int MAX_N = 1e5 +5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+
 using namespace std;
 
 int main (void){
-    
+
 }
