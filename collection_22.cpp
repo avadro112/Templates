416b4daaae59ecdb587fe1a87f01476049659ded
avadro112 <jkanant@gmail.com>
2023-01-02 12:52:07 +0530

    added

--- collection_22.cpp
+++ collection_22.cpp
@@ -7,6 +7,7 @@ const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 //debugs 
 #define deb(x) cout<<#x<<" : "<<endl;
+#define debu(...) 3000
 //loops 
 #define fo(i,n) for(int i=0;i<n;i++)
 #define kfor(i,k,n) for(int i=k;i<n;i++)
