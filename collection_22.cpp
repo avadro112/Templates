496681f67dee319d6875607398ee8b3d46658f91
avadro112 <jkanant@gmail.com>
2023-01-02 12:51:32 +0530

    defined deb(x)  and deb(...) for failure loops

--- collection_22.cpp
+++ collection_22.cpp
@@ -5,6 +5,11 @@
 const int MAX_N = 1e5 +5;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
+//debugs 
+#define deb(x) cout<<#x<<" : "<<endl;
+//loops 
+#define fo(i,n) for(int i=0;i<n;i++)
+#define kfor(i,k,n) for(int i=k;i<n;i++)
 
 
 using namespace std;
